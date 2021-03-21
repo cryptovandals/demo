70c8b364dae9d39887a1e3b4ef22442637f560b4
vrde <agranzot@mailbox.org>
2021-03-21 02:45:01 +0100

    Vandalize works yay

--- src/Vandalize.svelte
+++ src/Vandalize.svelte
@@ -8,6 +8,8 @@
   export let vandalizer: CryptoVandals;
 
   let logs: string[] = [];
+  let imageElement: HTMLImageElement;
+  let uploadElement: HTMLInputElement;
 
   function log(s: string) {
     console.log(s);
@@ -18,6 +20,9 @@
     log(`Stealing original image from ${fromToken.metadata.image}`);
     const imageReq = await fetch(fromToken.metadata.image);
     const image = await imageReq.blob();
+    await handleVandalize(image);
+
+    /*
     log(`Original image is ${image.size} bytes`);
     const cid = await ipfsAdd(image);
     log(`Image uploaded to IPFS, CID ${cid}`);
@@ -49,11 +54,72 @@
       fromToken.id,
       toGateway(metadataCid)
     );
+    */
   }
 
-  async function handleVandalize() {
-    await fromToken.contract.approve(vandalizer.address, fromToken.id);
+  async function handleVandalize(image: ArrayBuffer | Blob) {
+    let size: number;
+    if (image instanceof ArrayBuffer) {
+      size = image.byteLength;
+    } else {
+      size = image.size;
+    }
+    log(`Got image, size ${size} bytes`);
+    const cid = await ipfsAdd(image);
+    log(`Image uploaded to IPFS, CID ${cid}`);
+    log(`Making metadata PERMANENT! (remember: dolphins)`);
+    const metadata = {
+      ...fromToken.metadata,
+      image: toGateway(cid),
+    };
+    log(`New metadata`);
+    log(JSON.stringify(metadata, null, 4));
+    const metadataCid = await ipfsAdd(JSON.stringify(metadata, null, 4));
+    log(`Metadata uploaded to IPFS, CID ${metadataCid}`);
+
+    log("Ask our valued customer to sign the approve tx");
+    const approveTx = await fromToken.contract.approve(
+      vandalizer.address,
+      fromToken.id
+    );
+    log(`Approve tx hash ${approveTx.hash}`);
+    const approveReceipt = await approveTx.wait();
+    log(`Tx included in block ${approveReceipt.blockNumber}`);
+    log(
+      `${fromToken.contract.address}, ${fromToken.id}, ${toGateway(
+        metadataCid
+      )}`
+    );
+    await vandalizer.vandalize(
+      fromToken.contract.address,
+      fromToken.id,
+      toGateway(metadataCid)
+    );
+    //await fromToken.contract.approve(vandalizer.address, fromToken.id);
     //await vandalizer.vandalize(contract.address, id, tokenURI);
+  }
+
+  async function handleUpload() {
+    if (!uploadElement.files) {
+      return;
+    }
+    const fileReaderDataUrl = new FileReader();
+    const fileReaderBinary = new FileReader();
+    const [file] = uploadElement.files;
+
+    fileReaderDataUrl.onload = () => {
+      imageElement.src = fileReaderDataUrl.result as string;
+    };
+    fileReaderDataUrl.readAsDataURL(file);
+    fileReaderBinary.onload = async () => {
+      try {
+        await handleVandalize(fileReaderBinary.result as ArrayBuffer);
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    };
+    fileReaderBinary.readAsArrayBuffer(file);
   }
 </script>
 
@@ -74,7 +140,18 @@
     <pre>{JSON.stringify(fromToken.metadata, null, 4)}</pre>
   </div>
   <button on:click={() => handleLiberate()}>Liberate</button>
-  <button on:click={() => handleVandalize()}>Vandalize</button>
-  <img src={fromToken.metadata.image} alt={fromToken.metadata.description} />
+  <input
+    bind:this={uploadElement}
+    type="file"
+    id="image-file"
+    name="image"
+    on:change={(e) => handleUpload()}
+  />
+  <label for="image-file">Vandalize</label>
+  <img
+    bind:this={imageElement}
+    src={fromToken.metadata.image}
+    alt={fromToken.metadata.description}
+  />
   <pre>{logs.join('\n')}</pre>
 </div>
